83db9479c00b4ea21aa8f3891e60bed05730820d
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-03-17 21:44:17 -0300

    refactor: refatora lógica de sorteio e validação de campos no sorteador de números

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ const {log} = console;
 
 // declaração de array
 let quantidadeDeNumerosSorteados = [];
-
 // declaração do botão reiniciar
 const btnReiniciar = document.querySelector("#btn-reiniciar");
-
-// função que percorre uma faixa numerica comparando o número secreto.
-function faixaDeNumero(numeroSecreto, numeroInicial, numeroFinal) {
-    for(numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
-        if (numeroInicial === numeroSecreto) {
-            log(numeroInicial);
-            quantidadeDeNumerosSorteados.push(numeroInicial);
-        }
-    }
-    return quantidadeDeNumerosSorteados;
-}
 
 // função que gera o número secreto
 function gerarNumerosAleatorio(numeroFinal) {
@@ -26,19 +14,9 @@ function gerarNumerosAleatorio(numeroFinal) {
 
 // função que limpa os inputs de entrada.
 function limpaCampos() {
-    let quantidade = document.querySelector("#quantidade");
-    let inicial = document.querySelector("#de");
-    let final = document.querySelector("#ate");
-
-    if (quantidade) {
-        quantidade.value = "";
-    }
-    if (inicial) {
-        inicial.value = "";
-    }
-    if (final) {
-        final.value = "";
-    }
+    let quantidade = document.querySelector("#quantidade").value = '';
+    let inicial = document.querySelector("#de").value = '';
+    let final = document.querySelector("#ate").value = '';
 }
 
 // Lógica do botão reiniciar com chamada através do evento onclick
@@ -46,8 +24,8 @@ function reiniciar() {
     limpaCampos();
     quantidadeDeNumerosSorteados = [];
     document.querySelector('#resultado .texto__paragrafo').innerText = '';
-    btnReiniciar.classList.remove('container__botao');
-    btnReiniciar.classList.add('container__botao-desabilitado');
+    btnReiniciar.classList.toggle('container__botao');
+    btnReiniciar.classList.toggle('container__botao-desabilitado');
 
 }
 
@@ -64,11 +42,13 @@ function sortear() {
     
     // log(elementoNumeroFinal, elementoNumeroInicial, elementoQuantidadeDeNumeros);
     if (elementoQuantidadeDeNumeros <= 0 || elementoNumeroInicial <= 0 || elementoNumeroFinal <= 0) {
+        
         alert("Preencha os campos com valores maiores que 0!");
         elementoResultado.innerHTML = "Valores de entrada inconsistente...";
         limpaCampos();
         return;
-    } else if (elementoNumeroFinal <= elementoNumeroInicial && elementoQuantidadeDeNumeros <= 0 || elementoQuantidadeDeNumeros >= elementoNumeroFinal) {
+    } else if (elementoNumeroFinal <= elementoNumeroInicial || elementoQuantidadeDeNumeros <= 0 || elementoQuantidadeDeNumeros > (elementoNumeroFinal - elementoNumeroInicial + 1)) {
+        
         alert("Intervalo inválido!, campo 'até o número' não pode ser igual ou menor que o campo 'Do número', ou quantidade de números sorteados não pode ser menor ou igual a zero!, ou quantidade de número não pode ser maior que o número do campo 'até o número'!");
         elementoResultado.innerHTML = "Intervalo inválido...";
         limpaCampos();
@@ -77,16 +57,19 @@ function sortear() {
     } else {
 
         for(let quantidade = 0; quantidade < elementoQuantidadeDeNumeros; quantidade++) {
-            let numeroSecreto = gerarNumerosAleatorio(elementoNumeroFinal);
-            // log(numeroSecreto);
-            arrayNumerosSorteados = faixaDeNumero(numeroSecreto, elementoNumeroInicial, elementoNumeroFinal);
-        }
-        elementoResultado.innerText = '';
-        arrayNumerosSorteados.forEach(numero => {
-            elementoResultado.innerHTML += `<br>Números sorteados: ${numero}`;
-        });
+            let numeroSecreto;
 
-        btnReiniciar.classList.remove('container__botao-desabilitado');
-        btnReiniciar.classList.add('container__botao');
+            do {
+                numeroSecreto = gerarNumerosAleatorio(elementoNumeroFinal);
+            } while (arrayNumerosSorteados.includes(numeroSecreto) || numeroSecreto < elementoNumeroInicial);
+
+            arrayNumerosSorteados.push(numeroSecreto);
+        }
+        
+        elementoResultado.innerText = '';
+        elementoResultado.innerHTML = `<br>Números sorteados: ${arrayNumerosSorteados}<br>`;
+
+        btnReiniciar.classList.toggle('container__botao');
+        btnReiniciar.classList.toggle('container__botao-desabilitado');
     }
 } 
